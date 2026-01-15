713c7e1b320cb9f78e7193fb40493241b062bee1
Bernardo E. Dramos Jr. <dramosb44@gmail.com>
2026-01-16 01:06:41 +0530

    Create UniFace Test.py

---
+++ UniFace Test.py
@@ -0,0 +1,7 @@
+# Testing Procedures for UniFace
+
+# .\.venv\Scripts\python tools\detection.py --source "assets/bball 1.jpg"
+# .\.venv\Scripts\python tools\detection.py --source "assets/fball 1.png"
+# .\.venv\Scripts\python tools\detection.py --source "assets/fball 2.jpg"
+# .\.venv\Scripts\python tools\detection.py --source "assets/portrait 1.png"
+# .\.venv\Scripts\python tools\detection.py --source "assets/portrait 2.jpg"
